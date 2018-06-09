18d6a86b7a6cfc313fbe70c2f5938f2710238509
Patrik Palmér <patrik.palmer@gmail.com>
2018-06-09 18:52:08 +0200

    Thumb creator: Use RGB mode for thumbnails
    In order to support PNGs...

--- aws/lambda/thumbCreator/README.md
+++ aws/lambda/thumbCreator/README.md
@@ -4,6 +4,8 @@
 
 In order to create a deployment package for AWS lambda we need to go through a couple of manual steps. If docker is not installed, start by doing that.
 
+Create distribution
+
     docker run --rm -it -v "$PWD:/code" lambci/lambda:build-python2.7 sh
 	cd /code
 	virtualenv env
@@ -13,6 +15,8 @@ In order to create a deployment package for AWS lambda we need to go through a c
 	cp -R env/lib/python2.7/site-packages/psycopg2 ./
 	cp -R env/lib/python2.7/site-packages/python_dotenv-0.8.2.dist-info ./
 	zip -r dist.$(date +%Y-%m-%d).zip awslambda.py pixlcrypt_db.py thumbs.py PIL psycopg2 python_dotenv-0.8.2.dist-info
-	aws configure
-	aws s3 cp dist.$(date +%Y-%m-%d).zip s3://pixlcrypt-lambdas/thumbCreator/dist.$(date +%Y-%m-%d).zip
 	exit
+
+Upload to S3
+
+	aws s3 cp dist.$(date +%Y-%m-%d).zip s3://pixlcrypt-lambdas/thumbCreator/dist.$(date +%Y-%m-%d).zip
--- aws/lambda/thumbCreator/thumbs.py
+++ aws/lambda/thumbCreator/thumbs.py
@@ -2,16 +2,17 @@ from PIL import Image
 from PIL.ExifTags import TAGS
 
 def create_thumb(filename, obj, size, filepath):
-    img = Image.open(obj)
-    exif = img._getexif()
-    if exif != None:
-        for tag, value in exif.items():
-            decoded = TAGS.get(tag, tag)
-            if decoded == 'Orientation':
-                if value == 3: img = img.rotate(180)
-                if value == 6: img = img.rotate(270)
-                if value == 8: img = img.rotate(90)
-                break
+    img = Image.open(obj).convert('RGB')
+    if hasattr(img, '_getexif'):
+        exif = img._getexif()
+        if exif != None:
+            for tag, value in exif.items():
+                decoded = TAGS.get(tag, tag)
+                if decoded == 'Orientation':
+                    if value == 3: img = img.rotate(180)
+                    if value == 6: img = img.rotate(270)
+                    if value == 8: img = img.rotate(90)
+                    break
 
     img.thumbnail(size, Image.ANTIALIAS)
     img.save(filepath, format='JPEG', subsampling=0, quality=100)
