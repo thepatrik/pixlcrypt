b2988d6598cddc4efb5ec4bb5988f3b835ca53b1
Patrik Palmér <patrik.palmer@gmail.com>
2018-06-25 08:39:31 +0200

    Close db connections in finally clause

--- aws/lambda/thumbCreator/pixlcrypt_db.py
+++ aws/lambda/thumbCreator/pixlcrypt_db.py
@@ -1,7 +1,7 @@
 import psycopg2
 import os
 
-def connect():
+def _connect():
     host = os.environ['PGHOST']
     name = os.environ['PGDATABASE']
     user = os.environ['PGUSER']
@@ -10,45 +10,47 @@ def connect():
     cs = 'dbname=%s user=%s password=%s host=%s port=%s' % (name, user, password, host, port)
     return psycopg2.connect(cs)
 
-def commit_close(conn, cur=None):
+def _commit_close(conn, cur=None):
     conn.commit()
     if cur:
         cur.close()
 
-    conn.close()
-
 def get_user_id(email):
-    conn = connect()
-    cur = conn.cursor()
-    cur.execute("select id from pixlcrypt.user where email='" + email + "';")
-    id = str(cur.fetchone()[0])
-    commit_close(conn, cur)
+    conn = _connect()
+    try:
+        cur = conn.cursor()
+        cur.execute("select id from pixlcrypt.user where email='" + email + "';")
+        id = str(cur.fetchone()[0])
+        _commit_close(conn, cur)
+    finally:
+        conn.close()
+
     return id
 
 def insert_item(src, mime, content_type, user_id, date):
-    conn = connect()
-    cur = conn.cursor()
-    if date is not None:
-        cur.execute("insert into pixlcrypt.item(src, mime, content_type, user_id, created_at) values('" + src + "','" + mime + "','" + content_type + "','" + str(user_id) + "',to_timestamp(" + str(date) + ")) returning id;")
-    else:
-        cur.execute("insert into pixlcrypt.item(src, mime, content_type, user_id) values('" + src + "','" + mime + "','" + content_type + "','" + str(user_id) + "') returning id;")
+    conn = _connect()
+    try:
+        cur = conn.cursor()
+        if date is not None:
+            cur.execute("insert into pixlcrypt.item(src, mime, content_type, user_id, created_at) values('" + src + "','" + mime + "','" + content_type + "','" + str(user_id) + "',to_timestamp(" + str(date) + ")) returning id;")
+        else:
+            cur.execute("insert into pixlcrypt.item(src, mime, content_type, user_id) values('" + src + "','" + mime + "','" + content_type + "','" + str(user_id) + "') returning id;")
 
-    id = cur.fetchone()[0]
-    commit_close(conn, cur)
+        id = cur.fetchone()[0]
+        _commit_close(conn, cur)
+    finally:
+        conn.close()
+
     return id
 
 def insert_thumb(src, width, height, item_id):
-    conn = connect()
-    cur = conn.cursor()
-    cur.execute("insert into pixlcrypt.thumb(src, width, height, item_id) values('" + src + "','" + str(width) + "','" + str(height) + "','" + str(item_id) + "') returning id;")
-    id = cur.fetchone()[0]
-    commit_close(conn, cur)
+    conn = _connect()
+    try:
+        cur = conn.cursor()
+        cur.execute("insert into pixlcrypt.thumb(src, width, height, item_id) values('" + src + "','" + str(width) + "','" + str(height) + "','" + str(item_id) + "') returning id;")
+        id = cur.fetchone()[0]
+        _commit_close(conn, cur)
+    finally:
+        conn.close()
+
     return id
-
-def main():
-    user_id = get_user_id('pixlcrypt@gmail.com')
-    item_id = insert_item(src, 'image/jpeg', 'photo', user_id)
-    insert_thumb(thumb_src, width, height, item_id)
-
-if __name__ == '__main__':
-    main()
